166326e21377ec8368e01ee40a1c87e2834f41e3
aksingh <dev.ak.singh@outlook.com>
2022-03-21 17:31:06 +0530

    fix port error

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.port;
+let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
